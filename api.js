6b3210f147428ff7859ed1c0a58a955a2bdd3490
AnnaKap <anna-kapaeva1989@yandex.ru>
2024-08-01 19:33:22 +0300

    2.16 token

--- api.js
+++ api.js
@@ -8,29 +8,37 @@ export function getComments() {
 }
 
 
-export function postComments({ text, name }) {
+export function postComments({ text, name, token }) {
     return fetch("https://wedev-api.sky.pro/api/v2/anna-kapaeva/comments", {
         method: "POST",
         body: JSON.stringify({
             text: text,
             name: name,
         }),
-    }).then((response) => {
-      if(response.status === 201) {
-        return response.json();
-      } else{
-        return response.json().then((errorData) => {
-          if(response.status === 400) {
-            throw new Error("BadRequest: " + errorData.message);
-          } else if(response.status === 500) {
-            throw new Error("ServerUnavailable: Ошибка сервера");
-          } else{
-            throw new Error("ServerError: Сервер упал");
-          }
-        })
-      }
-      })
-}
+        headers: {'Authorization': `Bearer ${token}`}
+    })
+    //.then((response) => {
+      //if(response.status === 201) {
+       // return response.json();
+     // } else{
+        //return response.json().then((errorData) => {
+          //if(response.status === 400) {
+            //throw new Error("BadRequest: " + errorData.message);
+          //} else if(response.status === 500) {
+           // throw new Error("ServerUnavailable: Ошибка сервера");
+          //} else{
+           // throw new Error("ServerError: Сервер упал");
+          //}
+       // })
+     // }
+     // })
+//}
+
+postComments({
+  text:
+  name: user.user.name
+  token: user.user.token
+})
 
 // создать функцию login, которая принимает логин и пароль и делает запрос на сервер
 
